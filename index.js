e4a1e70bcd970fd7b564845226c12fd77c8f18e8
harshit3598 <68678320+harshit3598@users.noreply.github.com>
2021-10-18 23:44:44 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const authRoute = require('./routes/auth');
+const mongoose = require('mongoose');
+const postsPageRoute = require('./routes/posts');
+require('dotenv').config();
+
+mongoose.connect(process.env.DB_CONNECT, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    }).then(()=>console.log('Database Connected!!')).catch(err=>console.log("Auth failed"));
+
+app.use(express.json());
+
+app.use('/api/user', authRoute);
+app.use('/api/posts', postsPageRoute);
+
+app.listen(3000, ()=> console.log("Server running on localhost 3000"));
